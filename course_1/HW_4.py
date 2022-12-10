4cb8b65b2e854c8100ff96c2c4be7b3afac3b516
Dyno <0xDyno@gmail.com>
2022-12-10 22:34:45 +0800

    Python Basic HM #4

---
+++ course_1/HW_4.py
@@ -0,0 +1,59 @@
+from random import randint
+
+"""
+Task: Write the game "Guess the number", when writing the code, fulfill the following conditions:
+
+the program accepts only integers, if the number is a float - message the user and end the game
+output the number guessed by the user
+if the user entered a string - notify the user and end the game
+if the number is more than 10 - notify the user and end the game
+if the number is less than 1 - notify the user and end the game
+if the number is in the specified interval, but not guessed - inform the user that he "WAS CLOSE" and end the game
+if the user guessed the number - CONGRATULATIONS to him and end the game
+"""
+
+type_error = "It's %s, I accept only integer"
+wrong_range = "The number out of range 1-9"
+win_congrats = "Congratulations, you guesses the number, it's %d"
+lose_close = "Oh sad, you were so close"
+
+
+def is_float(line):
+	try:
+		float(line)
+		return True
+	except ValueError:
+		return False
+
+
+def play():
+	text = input("Guess the number: ")
+	try:
+		number = int(text)
+
+		if number < 1 or number > 9:
+			print(wrong_range)
+		else:
+			print("You wrote:", number)
+
+			if number == (r_numb := randint(1, 9)):
+				print(win_congrats % r_numb)
+			elif (number + 1) == r_numb or \
+				(number - 1) == r_numb:
+				print(lose_close)
+
+			print(f"My number: {number}, random: {r_numb}")
+
+	except ValueError:
+		text = text.lower()
+		if is_float(text):
+			print(type_error % "float")
+		else:
+			if text == "exit" or text == "e":
+				exit()
+			print(type_error % "text")
+
+
+if __name__ == '__main__':
+	while True:
+		play()
