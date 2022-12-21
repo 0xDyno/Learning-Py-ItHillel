3e6b6b92ce313293e0ed603a9008f5e9ceee42e4
Dyno <0xDyno@gmail.com>
2022-12-21 10:32:14 +0700

    Renaming Python Pro HW #4 to HW #3

---
+++ Py_Pro/lesson_5/lesson.py
@@ -0,0 +1,32 @@
+"""
+Lesson about OOP, basics
+
+Questions:
+- how to see code of basics functions -> list / len etc
+"""
+
+def first():
+    # shows in beautiful life
+    # 2 spaces after FROM
+    # 1 after WHERE
+    query = """SELECT *
+  FROM table_name
+ WHERE some_field = "K";"""
+    print(query)
+    
+    better_view = "SELECT *" \
+                  "\n  FROM table" \
+                  "\n WHERE name = \"Mimi\""
+    print(better_view)
+
+
+def second():
+    pass
+
+
+def third():
+    pass
+
+
+if __name__ == '__main__':
+    first()
