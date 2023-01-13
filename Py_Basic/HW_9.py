804def32afe0859dccde295272fa717e96787a83
Dyno <0xDyno@gmail.com>
2023-01-13 23:46:40 +0700

    Py Basic - HW_9 - Added Bonus Task (print the functions code in the file)

--- Py_Basic/HW_9.py
+++ Py_Basic/HW_9.py
@@ -1,31 +1,39 @@
 """
-Task 1
-Create a function that takes a list of numbers and returns a tuple. The first value of tuple is the average
-    value of all arguments, and the second value is the first number that is greater than the average.
-    
-    test_list = [1, 2, 3, 4, 12, 322, 55, 66, 71, 80, 94, 6, 10, 32]
-    
-Task 2
-Create a function that accepts 1 parameter - a list of numbers. The function returns a new list consisting only of
-    the even numbers that were in the argument.
+Part 1:
 
-Task 3
-Create a function that takes a list of tuples and returns the name of the item with the highest rating.
+    Task 1
+    Create a function that takes a list of numbers and returns a tuple. The first value of tuple is the average
+        value of all arguments, and the second value is the first number that is greater than the average.
+        
+        test_list = [1, 2, 3, 4, 12, 322, 55, 66, 71, 80, 94, 6, 10, 32]
+        
+    Task 2
+    Create a function that accepts 1 parameter - a list of numbers. The function returns a new list consisting only of
+        the even numbers that were in the argument.
+    
+    Task 3
+    Create a function that takes a list of tuples and returns the name of the item with the highest rating.
+    
+        grade = [('English', 88), ('Biology', 90), ('Math', 97), ('International language', 82)]
+        
+    Task 4
+    Create a function that takes the name of the award, looks it up in the film_awards dictionary and returns 1) the
+        year of the award 2) the name of the actor who was nominated for the award (if there was one) 3) type (Winner or
+        Nominee) You can use the work done from the previous DZ
+        
+        film_awards imported from HW_8_utils
+        
+    Task 5
+    Create a function that accepts a number and returns a list of tuples (film, rating) from the variable actor_films
+        whose rating is greater than this number argument. If there are no such films, inform the user about it, if the
+        number is more than 10, the user will be informed about it.
+    
+Part 3:
 
-    grade = [('English', 88), ('Biology', 90), ('Math', 97), ('International language', 82)]
-    
-Task 4
-Create a function that takes the name of the award, looks it up in the film_awards dictionary and returns 1) the
-    year of the award 2) the name of the actor who was nominated for the award (if there was one) 3) type (Winner or
-    Nominee) You can use the work done from the previous DZ
-    
-    film_awards imported from HW_8_utils
-    
-Task 5
-Create a function that accepts a number and returns a list of tuples (film, rating) from the variable actor_films
-    whose rating is greater than this number argument. If there are no such films, inform the user about it, if the
-    number is more than 10, the user will be informed about it.
+    Using the built-in inspect module, print out the code of each function you created.
 """
+import inspect
+
 from HW_8_utils import film_awards
 from HW_9_utils import actor_films
 
@@ -82,7 +90,7 @@ def task5(movies: dict, min_score: int) -> dict:
         message = f"Wow, we have a lot of movies for you = {len(res)}"
     else:
         message = None
-        # message = f"Found{len(res)} movie(s)"
+        # message = f"Found {len(res)} movie(s)"
         
     return {"message": message, "movies": res}
                 
@@ -114,3 +122,11 @@ for award in award_names:
 res5 = task5(movies=actor_films, min_score=2)
 print(res5.get("message")) if res5.get("message") is not None else None
 print(res5.get("movies"))
+
+# Part 3 - Task 1
+print()
+gl = globals().copy()
+for k, v in gl.items():
+    if inspect.isfunction(v):
+        print(inspect.getsource(v))
+    
